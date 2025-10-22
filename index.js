95d4bae63de4a4ab42a69a853cf4ff2d42fcda97
annisadiann <annisadian0412@gmail.com>
2025-10-22 11:21:39 +0700

    tambahkan koneksi ke database MySQL

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '@nnisa041204',
+        database: 'mahasiswa',
+        port : 3309
+    });
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:' + err.stack);
+        return;
+    }
+    console.log('Connected Succesfully');
+});
+
